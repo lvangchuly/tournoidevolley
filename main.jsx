dbe665d2bacfaf41920113c8a84cc1c6c65f0542
lvangchuly <64726354+lvangchuly@users.noreply.github.com>
2026-04-30 11:09:23 +0200

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,12 +1,10 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyDNTmvdTsyUQV4m1Nxno7jZZUQfeE-xY8I',
-  authDomain: 'tournoidevolley-8ab11.firebaseapp.com',
-  databaseURL: 'https://tournoidevolley-8ab11-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'tournoidevolley-8ab11',
-  storageBucket: 'tournoidevolley-8ab11.firebasestorage.app',
-  messagingSenderId: '891006950774',
-  appId: '1:891006950774:web:72a60a57d0d9c063636e57',
-  measurementId: 'G-VN74RDSXKM',
-};
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './styles.css';
 
-export const FIREBASE_DATABASE_URL = firebaseConfig.databaseURL;
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
